b3bcbf457d959f0ac0c4e0829b2e2a011b2a487c
prototypedave <popupgfj@gmail.com>
2025-05-01 20:27:24 +0300

    refactor: remove redundant code and optimize scraping match report

--- scraping/player/goal.py
+++ scraping/player/goal.py
@@ -1,54 +1,23 @@
 from .player import scrape_player_data
 from playwright.sync_api import Page
 from ..data_def.Match import GoalData, PlayerData
+from ..utils.util import safe_get_attribute, safe_get_text
 
-# ================================================================
-# Get player who scored
+# ===============================================================
+# get goal attributes
 
-# get_player_scored: (page: Page, event) -> PlayerData
-# interp. Returns PlayerData object of the player who scored by scraping
-#       the player link from the events locator.
+# get_goal_attributes: (event: Locator) -> dict
+# interp. cache all possible goal attributes
+#       - goal -> str        # scorer link
+#       - assist -> str      # assist link
+#       - pen -> str         # penality data
 
-def get_player_scored(page: Page, event) -> PlayerData:
-    try:
-        player_link = event.locator(".smv__playerName").get_attribute("href")
-        player = scrape_player_data(page, player_link)
-        return player
-    except Exception as e:
-        return None
-
-
-# =================================================================
-# Get player who assisted a goal
-
-# get_player_assisted: (page: Page, event) -> PlayerData
-# interp. Returns PlayerData object of the player who assisted by scraping
-#       the player link from the events locator.
-
-def get_player_assisted(page: Page, event) -> PlayerData:
-    try:
-        player_link = event.locator(".smv__assist").get_attribute("href")
-        player = scrape_player_data(page, player_link)
-        return player
-    except Exception as e:
-        return None
-
-
-# =================================================================
-# check if goal was a penalty
-
-# is_goal_penalty: Locator -> bool
-# checks if the goal was a penalty by checking the class of the element returns True 
-#       if it was a penalty else returns False
-
-def is_goal_penalty(event) -> bool:
-    try:
-        penalty = event.locator(".smv__subIncident").inner_text().strip()
-        if penalty:
-            return True if "penalty" in penalty else False
-    except Exception:
-        return False
-
+def get_goal_attributes(event) -> dict:
+    return {
+        "goal": safe_get_attribute(event, ".smv__playerName", "href"),
+        "assist": safe_get_attribute(event, ".smv__assist a", "href"),
+        "pen": safe_get_text(event, ".smv__subIncident")
+    }
 
 # =================================================================
 # Check if it was a goal and return goal data if it was
@@ -59,18 +28,17 @@ def is_goal_penalty(event) -> bool:
 #       returns GoalData object if it was a goal else returns None
 
 def get_goal(page: Page, event) -> GoalData:
-    try:
-        goal_check = event.locator(".smv__incidentIcon svg").get_attribute("data-testid")
-        if goal_check and "soccer" in goal_check:
-            scorer = get_player_scored(page, event)
+    goal_attributes = get_goal_attributes(event)
+    scorer_link = goal_attributes['goal']
 
-            if scorer is not None:
-                assist = get_player_assisted(page, event)
-                if assist is not None:
-                    return GoalData(scorer=scorer, assist=assist, penalty=False) 
-                else:
-                    return GoalData(scorer=scorer, assist=None, penalty=is_goal_penalty(event))
-            
-        return None
-    except Exception as e:
-        return None
+    if scorer_link is not None:
+        pen =  True if "penalty" in goal_attributes['pen'] else False
+        scorer = scrape_player_data(page, scorer_link)
+        assist_link = goal_attributes['assist']
+
+        if assist_link is not None:
+            assist = scrape_player_data(page, assist_link)
+        else:
+            assist = None
+
+        return GoalData(scorer=scorer, assist=assist, penality=pen)    
